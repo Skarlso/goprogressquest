3faafef4fe23cc41e4bb82477c7914c3f0bb9e35
Gergely Brautigam <skarlso777@gmail.com>
2015-11-12 19:36:45 +0100

    Fixed return JSON format.

--- gorpgapi.go
+++ gorpgapi.go
@@ -8,25 +8,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", Index)
-	log.Printf("Starting server to listen on port: 8989...")
-	http.ListenAndServe(":8989", nil)
-}
-
 // The main function which starts the rpg.
-func handler() {
+func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Printf("Starting server to listen on port: 8989...")
+	log.Fatal(http.ListenAndServe(":8989", router))
 }
 
 //Index Index request handler
 func Index(w http.ResponseWriter, r *http.Request) {
 	welcome := struct {
-		message string
+		//Message in order for it to be unmarshalled it must be exported
+		Message string `json:"message"`
 	}{
 		"Welcome!",
 	}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(welcome)
 }
