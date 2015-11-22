b9c7df47b4ce4c20bb8ab0adc01a07bfef8bffd2
Gergely Brautigam <skarlso777@gmail.com>
2015-11-22 16:00:37 +0100

    Added handling POST and some error handling for malformed data. It is not perfect yet.

--- gorpgapi.go
+++ gorpgapi.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,17 +16,20 @@ import (
 //APIVERSION Is the current API version
 const APIVERSION = "1"
 
+//READLIMIT 1 MiB read limit
+const READLIMIT = 1048576
+
 // The main function which starts the rpg
 func main() {
 	handlerChain := alice.New(Logging, PanicHandler)
 	router := mux.NewRouter().StrictSlash(true)
-	router.Handle("/api/"+APIVERSION+"/", handlerChain.ThenFunc(index))
-	router.Handle("/api/"+APIVERSION+"/create", handlerChain.ThenFunc(create))
+	router.Handle("/api/"+APIVERSION+"/", handlerChain.ThenFunc(index)).Methods("GET")
+	router.Handle("/api/"+APIVERSION+"/create", handlerChain.ThenFunc(create)).Methods("POST")
 	log.Printf("Starting server to listen on port: 8989...")
 	log.Fatal(http.ListenAndServe(":8989", router))
 }
 
-//index Index request handler
+//index a humble welcome to a new user
 func index(w http.ResponseWriter, r *http.Request) {
 	m := Message{}
 	m.Message = "Welcome to my RPG"
@@ -31,10 +38,30 @@ func index(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(m)
 }
 
-//register handling the creation of a new character
+//create handling the creation of a new character
 func create(w http.ResponseWriter, r *http.Request) {
+	var newName struct {
+		Name string `json:"name"`
+	}
 	ch := NewCharacter{}
-	ch.CharacterID = "Some Random Generated ID"
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, READLIMIT))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &newName); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	checkSum := sha1.Sum([]byte(newName.Name))
+	ch.CharacterID = fmt.Sprintf("%x", checkSum)
+	log.Printf("Created character sha hash: %v", ch.CharacterID)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(ch)
