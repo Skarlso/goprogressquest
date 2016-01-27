0175129f0955998bc23aff44d0a977b4c8cddbd4
Gergely Brautigam <skarlso777@gmail.com>
2016-01-27 20:26:47 +0100

    Increasing coverage #4.

--- adventure_test.go
+++ adventure_test.go
@@ -36,3 +36,42 @@ func TestStartingAdventuringForPlayerWhoIsAdventuring(t *testing.T) {
 
 	assert.Equal(t, resp.Body.String(), "{\"error\":\"Error occured, adventurer is already adventuring!\"}\n")
 }
+
+func TestErrorWhileBindingAdventurer(t *testing.T) {
+	mdb = TestDB{}
+	router := gin.New()
+	router.POST("/"+APIBASE+"/start", startAdventure)
+
+	req, _ := http.NewRequest("POST", "/"+APIBASE+"/start", strings.NewReader("invalid"))
+	req.Header.Add("Content-type", "application/json")
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, resp.Body.String(), "{\"error\":\"error while binding adventurer:invalid character 'i' looking for beginning of value\"}\n")
+}
+
+func TestStopAdventuringForACharacterWhichIsNotAdventuring(t *testing.T) {
+	mdb = TestDB{}
+	router := gin.New()
+	router.POST("/"+APIBASE+"/stop", stopAdventure)
+
+	req, _ := http.NewRequest("POST", "/"+APIBASE+"/stop", strings.NewReader("{\"id\":\"notonquest\"}"))
+	req.Header.Add("Content-type", "application/json")
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, resp.Body.String(), "{\"error\":\"Error occured, adventurer is not adventuring!\"}\n")
+}
+
+func TestStopAdventuringInvalidJSON(t *testing.T) {
+	mdb = TestDB{}
+	router := gin.New()
+	router.POST("/"+APIBASE+"/stop", stopAdventure)
+
+	req, _ := http.NewRequest("POST", "/"+APIBASE+"/stop", strings.NewReader("invalid"))
+	req.Header.Add("Content-type", "application/json")
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, resp.Body.String(), "{\"error\":\"error while binding adventurer:invalid character 'i' looking for beginning of value\"}\n")
+}
