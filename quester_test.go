300b612b60cc73d7dcd305a5e5d099d6947219c9
Gergely Brautigam <skarlso777@gmail.com>
2016-02-19 11:49:57 +0100

    Added a better condition test.

--- quester_test.go
+++ quester_test.go
@@ -10,6 +10,6 @@ func TestThatMonsterLevelCannotGoBelowZero(t *testing.T) {
 	ch := createCharacter("id", "test")
 	ch.Level = -10
 	monster := ch.spawnEnemy()
-
-	assert.Equal(t, monster.Level, 0, "Monster level did not match 0.")
+	assert.Condition(t, func() bool { return monster.Level >= 0 }, "Monster level was below 0.")
+	// assert.Equal(t, monster.Level, 0, "Monster level did not match 0.")
 }
